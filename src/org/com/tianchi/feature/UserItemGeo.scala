a6a42cef431b0ffe1a768c8f7f57ddb6926bdaec
liuqianghp1991 <liuqianghp1991@163.com>
2015-04-17 19:46:32 +0800

    添加了计算用户最近、最常用区域到商品的距离，对于没有给出位置的商品，使用对该商品进行的操作中的最常见局域

--- src/org/com/tianchi/feature/UserItemGeo.scala
+++ src/org/com/tianchi/feature/UserItemGeo.scala
@@ -16,16 +16,14 @@ class UserItemGeo(user_item_data: RDD[(String, Array[UserRecord])],
     ))
   }).filter(_._2.length > 0) //过滤数据
 
-  private val userData = user_item_cate_data_filtered.map { case (user_item_cate, records) => {
+  private val userData = user_item_cate_data_filtered.map { case (user_item_cate, records) =>
     val user_id = user_item_cate.split("_")(0)
     (user_id, records)
-  }
   }.reduceByKey((a, b) => a ++ b).cache()
 
-  private val itemData = user_item_cate_data_filtered.map { case (user_item_cate, records) => {
+  private val itemData = user_item_cate_data_filtered.map { case (user_item_cate, records) =>
     val item_id = user_item_cate.split("_")(1)
     (item_id, records)
-  }
   }.reduceByKey((a, b) => a ++ b).cache()
 
   val region_length = 5
@@ -34,18 +32,18 @@ class UserItemGeo(user_item_data: RDD[(String, Array[UserRecord])],
     geoHash.substring(0, region_length)
   }
 
-  def getItemRegions(): RDD[(String, String)] = {
+  //noinspection ComparingUnrelatedTypes
+  def getItemRegions: RDD[(String, String)] = {
     val item_selected_real_region = item_data.map(line => (line._1, line._2.map(p => toRegion(p.geoHash)).distinct.filter(!_.equals("")))).map {
-      case (item, records) => {
+      case (item, records) =>
         if (records.length > 0) (item, records.reduce((a, b) => a + "," + b))
         else (item, "")
-      }
     }.filter(!_._2.equals("")).collect().toMap
 
     //对于未给出位置的商品，以发生在其上的行为的地点形成的区域中出现次数最多的作为该商品所在区域
     val item_all_guess_region = itemData.map {
-      case (item_id, records) => {
-        val tmp = records.map(line => toRegion(line.geohash)).filter(!_.equals(""))
+      case (item_id, records) =>
+        val tmp = records.map(line => toRegion(line.geoHash)).filter(!_.equals(""))
         if (tmp.length == 0)
           (item_id, "")
         else {
@@ -54,25 +52,23 @@ class UserItemGeo(user_item_data: RDD[(String, Array[UserRecord])],
           }.maxBy(_._2)._1
           (item_id, value)
         }
-      }
     }
 
     item_all_guess_region.map {
-      case (item_id, region) => {
+      case (item_id, region) =>
         if (item_selected_real_region.contains(item_id))
           (item_id, item_selected_real_region(item_id))
         else
           (item_id, region)
-      }
     }
 
   }
 
   //求用户最近出现的地点
-  def getUserRecentlyRegion = {
+  def getUserRecentlyRegion(): RDD[(String, String)] = {
     userData.map {
       case (user_id, records) =>
-        val tmp: Array[String] = records.sortBy(_.time).map(p => toRegion(p.geohash)).filter(!_.equals(""))
+        val tmp: Array[String] = records.sortBy(_.time).map(p => toRegion(p.geoHash)).filter(!_.equals(""))
         if (tmp.length == 0)
           (user_id, "")
         else
@@ -81,10 +77,10 @@ class UserItemGeo(user_item_data: RDD[(String, Array[UserRecord])],
   }
 
   //求用户最常出现的区域
-  def getUserMostlyRegion = {
+  def getUserMostlyRegion() = {
     userData.map {
-      case (user_id, records) => {
-        val tmp = records.map(line => toRegion(line.geohash)).filter(!_.equals(""))
+      case (user_id, records) =>
+        val tmp = records.map(line => toRegion(line.geoHash)).filter(!_.equals(""))
         if (tmp.length == 0)
           (user_id, "")
         else {
@@ -93,7 +89,6 @@ class UserItemGeo(user_item_data: RDD[(String, Array[UserRecord])],
           }.maxBy(_._2)._1
           (user_id, value)
         }
-      }
     }
   }
 
@@ -107,32 +102,36 @@ class UserItemGeo(user_item_data: RDD[(String, Array[UserRecord])],
       if (s1(i) == s2(i)) count = count + 1
       else return count
     }
-    return count
+    count
   }
 
-  def getUserRecentlyItemDistance = {
-    val userRegion = getUserRecentlyRegion
+  def getUserItemDistance(f: () => RDD[(String, String)]) = {
+    val userRegion = f()
     user_item_cate_data_filtered.map {
-      case (user_item_cate, records) => {
+      case (user_item_cate, records) =>
         val user_id = user_item_cate.split("_")(0)
         val item_id = user_item_cate.split("_")(1)
         (user_id, item_id)
-      }
     }.join(userRegion).map {
-      case (user_id, (item_id, userRecentRegion)) =>
-        (item_id, (user_id, userRecentRegion))
-    }.join(getItemRegions()).map {
-      case (item_id, ((user_id, userRecentRegion), item_region)) =>
-        (user_id + "_" + item_id, )
-
+      case (user_id, (item_id, userregion)) =>
+        (item_id, (user_id, userregion))
+    }.join(getItemRegions).map {
+      case (item_id, ((user_id, userregion), item_region)) =>
+        val similarity = item_region.split(",").map(calRegionSimilarity(_, userregion)).max
+        (user_id + "_" + item_id, similarity)
     }
 
   }
 
-  def getUserMostlyItemDistance = {
-
+  def getUserRecentlyItemDistance = {
+    getUserItemDistance(getUserRecentlyRegion)
   }
 
-  def getUserNearlyItemDistance = {}
+  def getUserMostlyItemDistance = {
+    getUserItemDistance(getUserMostlyRegion)
+  }
 
+  def createUserItemGeoFeatures()={
+
+  }
 }
